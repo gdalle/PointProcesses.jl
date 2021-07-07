fb4658d39158b3f782999248da39a130226c33a2
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2021-07-07 13:08:05 +0200

    Fix doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,24 @@
 using PointProcesses
 using Documenter
 
-DocMeta.setdocmeta!(PointProcesses, :DocTestSetup, :(using PointProcesses); recursive=true)
+DocMeta.setdocmeta!(
+    PointProcesses,
+    :DocTestSetup,
+    :(using PointProcesses);
+    recursive = true,
+)
 
 makedocs(;
-    modules=[PointProcesses],
-    authors="Guillaume Dalle",
-    repo="https://github.com/gdalle/PointProcesses.jl/blob/{commit}{path}#{line}",
-    sitename="PointProcesses.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://gdalle.github.io/PointProcesses.jl",
-        assets=String[],
+    modules = [PointProcesses],
+    authors = "Guillaume Dalle",
+    repo = "https://github.com/gdalle/PointProcesses.jl/blob/{commit}{path}#{line}",
+    sitename = "PointProcesses.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://gdalle.github.io/PointProcesses.jl",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/gdalle/PointProcesses.jl",
-)
+deploydocs(; repo = "github.com/gdalle/PointProcesses.jl", devbranch = "main")
