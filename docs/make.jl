d0af942fc8dd4c52b7096ac971b5ec089ecd5063
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-01-16 10:31:01 +0100

    Update CI scripts

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,3 @@
-push!(LOAD_PATH, "../src/")
-
 using Documenter
 using DocumenterCitations
 using PointProcesses
@@ -12,13 +10,8 @@ makedocs(
     bib;
     modules=[PointProcesses],
     authors="Guillaume Dalle",
-    repo="https://github.com/gdalle/PointProcesses.jl/blob/{commit}{path}#{line}",
     sitename="PointProcesses.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://gdalle.github.io/PointProcesses.jl",
-        assets=String[],
-    ),
+    format=Documenter.HTML(),
     pages=["Home" => "index.md", "API reference" => "api.md"],
 )
 
