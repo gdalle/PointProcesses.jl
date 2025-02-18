be75db193f7699adbe3a7432a1df30e1a9517814
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2025-02-18 10:40:16 +0100

    specialize rand to PoissonProcess instead of AbstractPoissonProcess

--- src/poisson/simulation.jl
+++ src/poisson/simulation.jl
@@ -1,4 +1,19 @@
 function Base.rand(
+    rng::AbstractRNG, pp::PoissonProcess, tmin::T, tmax::T
+) where {T<:Real}
+    mark_dist = mark_distribution(pp)
+    N = rand(rng, Poisson(float(ground_intensity(pp) * (tmax - tmin))))
+    times = sort!(rand(rng, Uniform(tmin, tmax), N))
+    marks = [rand(rng, mark_dist) for n in 1:N]
+    return History(; times=times, marks=marks, tmin=tmin, tmax=tmax)
+end
+
+function Base.rand(pp::PoissonProcess, tmin::Real, tmax::Real)
+    return rand(default_rng(), pp, tmin, tmax)
+end
+
+#TODO : remove below
+function Base.rand(
     rng::AbstractRNG, pp::AbstractPoissonProcess, tmin::T, tmax::T
 ) where {T<:Real}
     mark_dist = mark_distribution(pp)
